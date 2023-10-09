8df8295fde9f3b4b595a681c965dd3d1b46f72fa
Erwann_Gallois <gallois.erwann@gmail.com>
2023-10-09 19:53:48 +0200

    Organisation donne dans les deux cas

--- orga_donnee.py
+++ orga_donnee.py
@@ -5,6 +5,10 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 train_data = pd.read_csv('Ressources/train.csv')
 test_data = pd.read_csv('Ressources/test.csv')
 
+# *----------------------------------------------------------------------------------*
+# *----------------------------Partie Train data-------------------------------------*
+# *----------------------------------------------------------------------------------*
+
 # Mise en forme Age
 train_data['Classe_Age'] = 'Inconnu'
 for i in range(len(train_data['Age'])):
@@ -42,13 +46,69 @@ for i in range(len(train_data['Age'])):
         rand = np.random.randint(1, 11)
         if (rand <= 4):
             train_data.loc[i, 'Age'] = np.random.randint(18, 30)
+            train_data.loc[i, 'Classe_Age'] = "Jeune"
         elif (rand > 4 and rand <= 7):
             train_data.loc[i, 'Age'] = np.random.randint(30, 50)
+            train_data.loc[i, 'Classe_Age'] = "Adulte"
         elif (rand > 7 and rand <= 9):
             train_data.loc[i, 'Age'] = np.random.randint(int(train_data['Age'].min()), 18)
+            train_data.loc[i, 'Classe_Age'] = "Enfant"
         else:
             train_data.loc[i, 'Age'] = np.random.randint(50, int(train_data['Age'].max()))
+            train_data.loc[i, 'Classe_Age'] = "Vieux"
 
 train_data = train_data.reindex (columns = ["PassengerId","Name","HomePlanet","Destination","CryoSleep","Cabin","Age","Classe_Age","VIP","RoomService","FoodCourt","ShoppingMall","Spa","VRDeck", "Argent_Total", "Famille", "Transported"])
 train_data.to_csv('Ressources/train2.csv', index=False)
-print("Done")
+print("Done pour train data")
+
+# *----------------------------------------------------------------------------------*
+# *----------------------------Partie Test data--------------------------------------*
+# *----------------------------------------------------------------------------------*
+
+test_data['Classe_Age'] = 'Inconnu'
+for i in range(len(test_data['Age'])):
+    if (test_data['Age'][i] > 0 and test_data['Age'][i] < 18):
+        test_data.loc[i, 'Classe_Age'] = "Enfant"
+    elif (test_data['Age'][i] >= 18 and test_data['Age'][i] < 30):
+        test_data.loc[i, 'Classe_Age'] = "Jeune"
+    elif (test_data['Age'][i] >= 30 and test_data['Age'][i] < 50):
+        test_data.loc[i, 'Classe_Age'] = "Adulte"
+    elif (test_data['Age'][i] >= 50):
+        test_data.loc[i, 'Classe_Age'] = "Vieux"
+
+print(test_data.value_counts('Classe_Age'))
+
+test_data["RoomService"].fillna(test_data["RoomService"].median(), inplace= True)
+test_data["FoodCourt"].fillna(test_data["FoodCourt"].median(), inplace= True)
+test_data["ShoppingMall"].fillna(test_data["ShoppingMall"].median(), inplace= True)
+test_data["Spa"].fillna(test_data["Spa"].median(), inplace= True)
+test_data["VRDeck"].fillna(test_data["VRDeck"].median(), inplace= True)
+
+test_data["Argent_Total"] = test_data["RoomService"] + test_data["FoodCourt"] + test_data["ShoppingMall"] + test_data["Spa"] + test_data["VRDeck"]
+# Ajout Colonne Famille
+test_data['Famille'] = False
+for i in range(len(test_data['Cabin'])-1):
+    if (test_data['Cabin'][i] == test_data['Cabin'][i+1]):
+        test_data.loc[i , "Famille"] = True
+
+
+test_data["Argent_Total"] = test_data["RoomService"] + test_data["FoodCourt"] + test_data["ShoppingMall"] + test_data["Spa"] + test_data["VRDeck"]
+for i in range(len(test_data['Age'])):
+    if (test_data['Classe_Age'][i] == "Inconnu"):
+        rand = np.random.randint(1, 11)
+        if (rand <= 4):
+            test_data.loc[i, 'Age'] = np.random.randint(18, 30)
+            test_data.loc[i, 'Classe_Age'] = "Jeune"
+        elif (rand > 4 and rand <= 7):
+            test_data.loc[i, 'Age'] = np.random.randint(30, 50)
+            test_data.loc[i, 'Classe_Age'] = "Adulte"
+        elif (rand > 7 and rand <= 9):
+            test_data.loc[i, 'Age'] = np.random.randint(int(test_data['Age'].min()), 18)
+            test_data.loc[i, 'Classe_Age'] = "Enfant"
+        else:
+            test_data.loc[i, 'Age'] = np.random.randint(50, int(test_data['Age'].max()))
+            test_data.loc[i, 'Classe_Age'] = "Vieux"
+
+test_data = test_data.reindex (columns = ["PassengerId","Name","HomePlanet","Destination","CryoSleep","Cabin","Age","Classe_Age","VIP","RoomService","FoodCourt","ShoppingMall","Spa","VRDeck", "Argent_Total", "Famille", "Transported"])
+test_data.to_csv('Ressources/test2.csv', index=False)
+print("Done pour test data")
