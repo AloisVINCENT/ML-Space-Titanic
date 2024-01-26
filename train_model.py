43d8f89869c1c612223b0b720e0a7d94a782f52c
Erwann.Gallois <gallois.erwann@gmail.com>
2024-01-26 09:40:26 +0100

    Refactor data preprocessing and add print statements for prediction
    Signed-off-by: Erwann.Gallois <gallois.erwann@gmail.com>

--- train_model.py
+++ train_model.py
@@ -11,7 +11,19 @@ test_data = pd.read_csv('Ressources/test.csv')
 ### HomePlanet, CryoSleep, Destination, Age, VIP, Argent_Total
 
 train_data = train_data.reindex (columns = ["PassengerId","Name","HomePlanet","Destination","CryoSleep","Cabin","Age","Classe_Age","VIP","RoomService","FoodCourt","ShoppingMall","Spa","VRDeck", "Argent_Total", "Famille", "Transported"])
-features = ["CryoSleep", "Destination", "VIP", "HomePlanet"]
+col_to_sum = ["RoomService","FoodCourt","ShoppingMall","Spa","VRDeck"]
+for col in col_to_sum:
+    train_data[col] = train_data[col].fillna(0)
+    test_data[col] = test_data[col].fillna(0)
+    train_data[col] = train_data[col].astype(int)
+    test_data[col] = test_data[col].astype(int)
+train_data["Argent_Total"] = train_data[col_to_sum].sum(axis=1)
+features = ["Age", "CryoSleep", "Destination", "VIP", "HomePlanet", "Argent_Total"]
+train_data = train_data.reindex (columns = ["PassengerId","Name","HomePlanet","Destination","CryoSleep","Cabin","Age","Classe_Age","VIP","RoomService","FoodCourt","ShoppingMall","Spa","VRDeck", "Argent_Total", "Famille", "Transported"])
+test_data = test_data.reindex (columns = ["PassengerId","Name","HomePlanet","Destination","CryoSleep","Cabin","Age","Classe_Age","VIP","RoomService","FoodCourt","ShoppingMall","Spa","VRDeck", "Argent_Total", "Famille"])
+
+train_data[features] = train_data[features].fillna(0)
+test_data[features] = test_data[features].fillna(0)
 X = pd.get_dummies(train_data[features])
 y = train_data["Transported"]
 
@@ -23,8 +35,9 @@ forest_model = RandomForestClassifier(n_estimators= 2100, max_depth = 6, random_
 forest_model.fit(X_train, y_train)
 
 # Prédiction sur l'ensemble de test
+print("\nPredicting using RandomForest...")
 forest_pred = forest_model.predict(X_test)
-
+print("Done predicting using RandomForest.\n")
 # Évaluation de RandomForest
 print("Performance de RandomForest")
 print("Précision :", accuracy_score(y_test, forest_pred))
